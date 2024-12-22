c3ec0d2a5f5f577ecc2f3a2bee762787e623ccd8
Thanas-Fuqi <thanas123fuqi@gmail.com>
2024-12-22 10:35:06 +0100

    Added banners

--- logic/database.js
+++ logic/database.js
@@ -1,8 +1,41 @@
 ﻿const data = [
   {
     "country": "Shtetet e Bashkuara",
-    "code": "USA",
     "color": "green",
+    "name": ["Penny", "Dollar", "pseudo"],
+    "coins": [[
+      {
+        "0": "text 1",
+        "1": "text 2",
+      },
+      {
+        "0": "text 3",
+        "1": "text 4",
+      },
+    ], [
+      {
+        "0": "text 5",
+        "1": "text 6",
+      },
+      {
+        "0": "text 7",
+        "1": "text 8",
+      },
+    ], [
+      {
+        "0": "text 9",
+        "1": "text 10",
+      },
+      {
+        "0": "text 11",
+        "1": "text 12",
+      },
+    ],
+    ]
+  },
+  {
+    "country": "Ukrainw",
+    "color": "yellow",
     "name": ["Penny", "Dollar", "pseudo"],
     "coins": [[
       {
